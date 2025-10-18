1a35d1d80ce3728662ab11cd0ac93183a91d06b2
antoinetordeux <38569781+antoinetordeux@users.noreply.github.com>
2025-10-18 22:47:11 +0200

    Delete install.R

--- install.R
+++
@@ -1,7 +0,0 @@
-# install.R
-
-# Install required packages
-install.packages(c("IRkernel", "ggplot2", "factoextra", "palmerpenguins","MASS", "fmsb", "dplyr", "psych","FactoMineR", "corrplot", "mclust", "pracma","aplpack"))
-
-# Register IRkernel
-IRkernel::installspec(user = FALSE)
